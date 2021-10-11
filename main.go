4d9954c6f800770ed9881aa2b28843dc25d76696
Craig Sharpe <Craig@Sharpestcomputers.co.uk>
2021-10-11 22:01:08 +0100

    Updated to use affected squares struct

--- main.go
+++ main.go
@@ -11,13 +11,18 @@ import (
 )
 
 var board [8][8]string
+var player string = "B"
+
+type affectedSquares struct {
+	X, Y int
+}
 
 func main() {
 	fmt.Println("Starting")
 
 	boardSetup()
 	fmt.Println(board)
-	printBoard()
+	printBoard(board)
 
 	pixelgl.Run(run)
 
@@ -33,14 +38,286 @@ func run() {
 	if err != nil {
 		panic(err)
 	}
-	win.Clear(colornames.Skyblue)
 
+	drawBoard := drawBoard()
+	grid := drawGrid()
+
+	//circle := drawCircle(4, 4, colornames.Black)
+	//circle2 := drawCircle(4, 5, colornames.White)
+
+	// ... later in the code
+	for !win.Closed() {
+		// ...
+		win.Clear(colornames.Skyblue)
+		drawBoard.Draw(win)
+		grid.Draw(win)
+		//circle.Draw(win)
+		//circle2.Draw(win)
+		refreshBoard(win)
+
+		if win.JustPressed(pixelgl.MouseButtonLeft) {
+			// Which square was clicked on?
+			x, y := getClickedBox(win.MousePosition())
+
+			// Check if player made a valid move
+			affected := findAffectedSquares(player, x-1, y-1)
+			if len(affected) > 0 {
+
+				flipAffectedSquares(affected)
+				board[x-1][y-1] = player
+
+				// Check if next player can move
+				nextPlayer := ""
+				switch player {
+				case "B":
+					nextPlayer = "W"
+				case "W":
+					nextPlayer = "B"
+				}
+				valid := validMoves(nextPlayer)
+				if len(valid) > 0 {
+					player = nextPlayer
+				}
+			}
+
+		}
+
+		win.Update()
+		// ...
+	}
+}
+
+func validMoves(p string) []affectedSquares {
+	possibleMoves := []affectedSquares{}
+	for x := 0; x < 8; x++ {
+		for y := 0; y < 8; y++ {
+			if board[x][y] == " " {
+				chkSquare := findAffectedSquares(p, x, y)
+				if len(chkSquare) > 0 {
+					possibleMoves = append(possibleMoves, chkSquare...)
+				}
+			}
+		}
+	}
+	return possibleMoves
+}
+
+func flipAffectedSquares(affected []affectedSquares) {
+	for _, square := range affected {
+		switch board[square.X][square.Y] {
+		case "B":
+			board[square.X][square.Y] = "W"
+		case "W":
+			board[square.X][square.Y] = "B"
+		}
+
+	}
+}
+func findAffectedSquares(p string, x, y int) []affectedSquares {
+	if board[x][y] != " " {
+		return []affectedSquares{}
+	}
+	AllSquaresAffected := []affectedSquares{}
+
+	// Check left
+	affected := []affectedSquares{}
+chkLeft:
+	for rowIndex := x + 1; rowIndex < 8; rowIndex++ {
+		fmt.Printf("Checking Left X: %d, Y: %d = %s (p: %s)\n", x, y, board[rowIndex][y], p)
+		switch board[rowIndex][y] {
+		case " ":
+			break chkLeft
+		case p:
+			AllSquaresAffected = append(AllSquaresAffected, affected...)
+			break chkLeft
+		default:
+			affected = append(affected, affectedSquares{X: rowIndex, Y: y})
+		}
+	}
+
+	// Check Right
+	affected = []affectedSquares{}
+chkRight:
+	for rowIndex := x - 1; rowIndex > 0; rowIndex-- {
+		fmt.Printf("Checking Right X: %d, Y: %d = %s (p: %s)\n", x, y, board[rowIndex][y], p)
+		switch board[rowIndex][y] {
+		case " ":
+			break chkRight
+		case p:
+			AllSquaresAffected = append(AllSquaresAffected, affected...)
+			break chkRight
+		default:
+			affected = append(affected, affectedSquares{X: rowIndex, Y: y})
+		}
+	}
+
+	// Check Up
+	affected = []affectedSquares{}
+chkUp:
+	for colIndex := y + 1; colIndex < 8; colIndex++ {
+		fmt.Printf("Checking Up X: %d, Y: %d = %s (p: %s)\n", x, y, board[x][colIndex], p)
+		switch board[x][colIndex] {
+		case " ":
+			break chkUp
+		case p:
+			AllSquaresAffected = append(AllSquaresAffected, affected...)
+			break chkUp
+		default:
+			affected = append(affected, affectedSquares{X: x, Y: colIndex})
+		}
+	}
+
+	// Check Dowm
+	affected = []affectedSquares{}
+chkDown:
+	for colIndex := y - 1; colIndex > 0; colIndex-- {
+		fmt.Printf("Checking Down X: %d, Y: %d = %s (p: %s)\n", x, y, board[x][colIndex], p)
+		switch board[x][colIndex] {
+		case " ":
+			break chkDown
+		case p:
+			AllSquaresAffected = append(AllSquaresAffected, affected...)
+			break chkDown
+		default:
+			affected = append(affected, affectedSquares{X: x, Y: colIndex})
+		}
+	}
+
+	// Check DiagUpRight
+	affected = []affectedSquares{}
+
+	colIndex := y
+	rowIndex := x
+DiagUpRight:
+	for colIndex < 7 && rowIndex < 7 {
+		colIndex++
+		rowIndex++
+		fmt.Printf("Checking DiagUpRight X: %d, Y: %d = %s (p: %s)\n", rowIndex, colIndex, board[rowIndex][colIndex], p)
+		switch board[rowIndex][colIndex] {
+		case " ":
+			break DiagUpRight
+		case p:
+			AllSquaresAffected = append(AllSquaresAffected, affected...)
+			break DiagUpRight
+		default:
+			affected = append(affected, affectedSquares{X: rowIndex, Y: colIndex})
+		}
+	}
+
+	// Check DiagDownLeft
+	affected = []affectedSquares{}
+
+	colIndex = y
+	rowIndex = x
+DiagDownLeft:
+	for colIndex > 0 && rowIndex > 0 {
+		colIndex--
+		rowIndex--
+		fmt.Printf("Checking DiagDownLeft X: %d, Y: %d = %s (p: %s)\n", rowIndex, colIndex, board[rowIndex][colIndex], p)
+		switch board[rowIndex][colIndex] {
+		case " ":
+			break DiagDownLeft
+		case p:
+			AllSquaresAffected = append(AllSquaresAffected, affected...)
+			break DiagDownLeft
+		default:
+			affected = append(affected, affectedSquares{X: rowIndex, Y: colIndex})
+		}
+	}
+
+	// Check DiagUpLeft
+	affected = []affectedSquares{}
+
+	colIndex = y + 1
+	rowIndex = x - 1
+DiagUpLeft:
+	for colIndex < 7 && rowIndex > 0 {
+		colIndex++
+		rowIndex--
+		fmt.Printf("Checking DiagUpLeft X: %d, Y: %d = %s (p: %s)\n", rowIndex, colIndex, board[rowIndex][colIndex], p)
+		switch board[rowIndex][colIndex] {
+		case " ":
+			break DiagUpLeft
+		case p:
+			AllSquaresAffected = append(AllSquaresAffected, affected...)
+			break DiagUpLeft
+		default:
+			affected = append(affected, affectedSquares{X: rowIndex, Y: colIndex})
+		}
+	}
+
+	// Check DiagDownRight
+	affected = []affectedSquares{}
+
+	colIndex = y
+	rowIndex = x
+DiagDownRight:
+	for colIndex > 0 && rowIndex < 7 {
+		colIndex--
+		rowIndex++
+		fmt.Printf("Checking DiagDownRight X: %d, Y: %d = %s (p: %s)\n", rowIndex, colIndex, board[rowIndex][colIndex], p)
+		switch board[rowIndex][colIndex] {
+		case " ":
+			break DiagDownRight
+		case p:
+			AllSquaresAffected = append(AllSquaresAffected, affected...)
+			break DiagDownRight
+		default:
+			affected = append(affected, affectedSquares{X: rowIndex, Y: colIndex})
+		}
+	}
+
+	fmt.Printf("Affected: %+v\n", AllSquaresAffected)
+	return AllSquaresAffected
+}
+
+func updatePlayer(p string) string {
+	if p == "B" {
+		return "W"
+	} else {
+		return "B"
+	}
+}
+
+func getClickedBox(vect pixel.Vec) (int, int) {
+	x := ((vect.X - 50) / 75)
+	y := ((vect.Y - 50) / 75)
+	fmt.Println(x, y)
+	if x > 8 || x < 0 {
+		return 0, 0
+	}
+	if y > 8 || y < 0 {
+		return 0, 0
+	}
+	return int(x + 1), int(y + 1)
+}
+
+func refreshBoard(win *pixelgl.Window) {
+
+	for x := 0.0; x < 8; x++ {
+		for y := 0.0; y < 8; y++ {
+
+			switch board[int(x)][int(y)] {
+			case "W":
+				drawCircle(x+1, y+1, colornames.White).Draw(win)
+			case "B":
+				drawCircle(x+1, y+1, colornames.Black).Draw(win)
+			}
+
+		}
+	}
+}
+
+func drawBoard() *imdraw.IMDraw {
 	board := imdraw.New(nil)
 	board.Color = colornames.Lightgreen
 	board.Push(pixel.V(50, 50))
 	board.Push(pixel.V(650, 650))
 	board.Rectangle(0)
+	return board
+}
 
+func drawGrid() *imdraw.IMDraw {
 	grid := imdraw.New(nil)
 	grid.Color = colornames.Black
 	for x := 0.0; x < 600; x = x + 75 {
@@ -50,22 +327,7 @@ func run() {
 			grid.Rectangle(1)
 		}
 	}
-
-	circle := drawCircle(4, 4, colornames.Black)
-	circle2 := drawCircle(4, 5, colornames.White)
-
-	// ... later in the code
-	for !win.Closed() {
-		// ...
-		win.Clear(colornames.Aliceblue)
-		board.Draw(win)
-		grid.Draw(win)
-		circle.Draw(win)
-		circle2.Draw(win)
-
-		win.Update()
-		// ...
-	}
+	return grid
 }
 
 func drawCircle(x, y float64, c color.Color) *imdraw.IMDraw {
@@ -88,11 +350,11 @@ func boardSetup() {
 	board[4][4] = "B"
 }
 
-func printBoard() {
+func printBoard(b [8][8]string) {
 	for x := 0; x < 8; x++ {
 		fmt.Printf("\n")
 		for y := 0; y < 8; y++ {
-			switch board[x][y] {
+			switch b[x][y] {
 			case " ":
 				fmt.Printf(".")
 			case "B":
